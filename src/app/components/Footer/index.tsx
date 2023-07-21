6bc36d919c06de22b137197485874dcb9c88db18
Jason <yulaie1012@gmail.com>
2023-07-22 00:11:12 +0800

    feat: Add scroll-to-top button in Footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,12 +1,19 @@
+import { HiArrowUp } from 'react-icons/hi';
+
+import { ScrollLink } from '@/components/ui';
+
 import SocialMediaLinks from './SocialMediaLinks';
 
 const Footer = function () {
   return (
-    <footer className="border-t p-4 sm:px-20 md:flex md:flex-row md:justify-between">
+    <footer className="relative border-t p-4 sm:px-20 md:flex md:flex-row md:justify-between">
       <div className="mb-2 text-center text-lg text-neutral-500 dark:text-neutral-100 md:mb-0">© 2023 Jason Luo</div>
       <div className="flex flex-row items-center justify-center space-x-3">
         <SocialMediaLinks />
       </div>
+      <ScrollLink to="hero" className="absolute left-[50%] right-[50%] top-[-45px]">
+        <HiArrowUp size={35} className="animate-bounce" />
+      </ScrollLink>
     </footer>
   );
 };
